66abe63063c3af875ba16c133e7ee824fb7df8eb
Matt Hough <matt@hough.tv>
2017-09-10 21:00:19 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,8 +2,9 @@ class Dog
   def name #reads name of the dog from instance variable @name
     @name
   end
-
   def name=(new_name) #writes the name of the dog to instance variable @name
-    @name = new_name
+    @name = new_name  
   end
+
+  
 end
